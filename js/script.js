12bdc513c43a09cd6a291dfd94f9e3181950acff
Wu Han <wuhanstudio@hust.edu.cn>
2020-08-23 17:38:15 +0800

    Add Bootstrap Homepage

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ $(document).ready(function() {
+    // Init Scrollspy
+    $('body').scrollspy({
+        target:'#main-nav',
+        offset: 100
+    });
+
+    // Smooth Scrolling
+    $("#main-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 50
+        }, 800, function () {
+            // window.location.hash = hash;
+        });
+        }
+    }); 
+});
